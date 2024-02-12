807b207b3f23b00855f50ab16cf2d3d2d21fc297
ESUM BRANDON <esumbrandon074@gmail.com>
2024-02-12 13:17:54 +0100

    Minor updates

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:user_repository/user_repository.dart';
 
 class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+  final UserRepository userRepository;
+  const Homepage(this.userRepository, {super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/home_page.dart';
+import 'package:user_repository/user_repository.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'food app',
-      home: Homepage(),
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(Homepage(FirebaseUserRepo()));
 }
